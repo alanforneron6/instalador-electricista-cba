ed6e8469c619cc212ecf1771b5ae52d4a2df3df1
Alan Forneron <alan.forneron@naranja.com>
2026-09-24 20:09:57 -0300

    creado proyecto inicial iOS

---
+++ InstaladorElectricistaCBA/InstaladorElectricistaCBA/InstaladorElectricistaCBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaladorElectricistaCBAApp.swift
+//  InstaladorElectricistaCBA
+//
+//  Created by Alan Forneron on 24/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct InstaladorElectricistaCBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstaladorElectricistaCBA/InstaladorElectricistaCBATests/InstaladorElectricistaCBATests.swift
@@ -0,0 +1,18 @@
+//
+//  InstaladorElectricistaCBATests.swift
+//  InstaladorElectricistaCBATests
+//
+//  Created by Alan Forneron on 24/09/2026.
+//
+
+import Testing
+
+struct InstaladorElectricistaCBATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
